2164549b2191db4e31a90975f573b95b9ea9c0f3
risano <muhammadrisano@gmail.com>
2021-08-13 17:14:48 +0700

    add comment

--- pages/detail.js
+++ pages/detail.js
@@ -3,10 +3,7 @@ import {useRouter} from 'next/router'
 import { useEffect } from "react"
 
 function Datail() {
-  const router = useRouter()
-  useEffect(()=>{
-    console.log(router);
-  },[])
+  
   return (
     <Layout title={router.pathname}>
       <h1>halaman detail</h1>
